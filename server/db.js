5e18ad79090c8256e0a00ef6fa0dc9674d981746
Karl Hernandez <karlp.hernandez@gmail.com>
2021-07-20 00:09:51 +0000

    Revert password changes

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "admin", // change when done
+    password: "password",
     host: "localhost",
     port: 5432,
     database: "main"
