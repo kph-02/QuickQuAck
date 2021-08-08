ff8574da39e6eb0a62a1b2055fbc2f2b9794bf41
Ajay <ajayboooter@gmail.com>
2021-08-07 21:03:44 -0700

    styling update

--- QQLogin/components/PostMenu.js
+++ QQLogin/components/PostMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, StyleSheet, Text, Image, TouchableHighlight } from 'react-native';
+import { Dimensions, StyleSheet, Text, Image, TouchableHighlight, View } from 'react-native';
 import { Menu, MenuOptions, MenuOption, MenuTrigger, renderers } from 'react-native-popup-menu';
 
 const { SlideInMenu } = renderers;
@@ -13,26 +13,27 @@ const PostMenu = ({ navigation }) => {
       </MenuTrigger>
 
       {/* Two menu options: Text post and Poll post */}
-      <MenuOptions style={styles.container}>
+      <MenuOptions>
         {/* Title */}
         <MenuOption style={styles.title}>
-          <Text style={styles.text}>Post to QuickQuAck</Text>
+          <Text>Post to QuickQuAck</Text>
         </MenuOption>
 
-        {/* Text post */}
-        <MenuOption
-          style={{ paddingVertical: 10 }}
-          onSelect={() => {
-            navigation.navigate('Create Post');
-          }}
-        >
-          <Text style={styles.text}>Text</Text>
-        </MenuOption>
-
-        {/* Poll post */}
-        <MenuOption style={{ paddingVertical: 10 }} onSelect={() => alert('W.I.P')}>
-          <Text style={styles.text}>Poll</Text>
-        </MenuOption>
+        <View style={styles.container}>
+          {/* Text post */}
+          <MenuOption
+            style={{ paddingVertical: 10 }}
+            onSelect={() => {
+              navigation.navigate('Create Post');
+            }}
+          >
+            <Text style={styles.text}>Text</Text>
+          </MenuOption>
+          {/* Poll post */}
+          <MenuOption style={{ paddingVertical: 10 }} onSelect={() => alert('W.I.P')}>
+            <Text style={styles.text}>Poll</Text>
+          </MenuOption>
+        </View>
       </MenuOptions>
     </Menu>
   );
@@ -41,30 +42,25 @@ const PostMenu = ({ navigation }) => {
 const { width, height } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
-  title: {},
+  title: {
+    alignItems: 'center',
+    padding: 10,
+    borderBottomColor: 'black',
+    borderWidth: 1,
+  },
   container: {
     flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white',
   },
-  modal: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor : "#00BCD4",
-    backgroundColor: 'yellow',
-    height: '20%',
-    width: '80%',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-    marginTop: 80,
-    marginLeft: 40,
-  },
   text: {
-    marginLeft: 30,
+    padding: width / 5,
     color: 'black',
     fontSize: height * 0.019,
+    borderWidth: 1,
+    borderColor: 'black',
   },
   touchableStyle: {
     position: 'absolute',
