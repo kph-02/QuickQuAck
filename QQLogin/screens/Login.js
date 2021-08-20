c28d43e160d1f8a84f47fdd9acb65c105302e90c
NickHale01 <76475851+NickHale01@users.noreply.github.com>
2021-08-20 09:14:21 -0700

    Update Login.js

--- QQLogin/screens/Login.js
+++ QQLogin/screens/Login.js
@@ -20,7 +20,7 @@ import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 // const serverIp = '192.168.1.51';
 // const serverIp = '100.83.38.217';
 // const serverIp = '10.128.124.246';
-const serverIp = '132.249.242.71';
+const serverIp = ' 10.128.124.246/25';
 
 import {
   StyledContainer,
