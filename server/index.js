b4fe547d0a95de5b25e377ed8b3e7d6350af153f
jillsu1114 <jillsu1114@gmail.com>
2021-08-23 11:00:13 -0700

    Websocket Implementation WIP

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const express = require("express"); //import Express
 const app = express(); //create an Express application on the app variable
 const cors = require("cors"); //import cors
 
+//socket.io related items
+const socketPort = 8000;
+const { emit } = require("process");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+   cors: {
+      origin: "http://localhost:5001",
+      methods: ["GET", "POST", "PUT"],
+   },
+});
+
+
 // middleware //
 app.use(express.json()); //req.body
 app.use(cors());
@@ -23,3 +35,33 @@ app.use("/chat", require("./routes/chat"));
 app.listen(5000, () => {
     console.log("server is running on port 5000");
 });
+
+
+
+// sends out the 10 most recent messages from recent to old
+// const emitMostRecentMessages = () => {
+//    db.getSocketMessages()
+//       .then((result) => io.emit("chat message", result))
+//       .catch(console.log);
+// };
+// connects, creates message, and emits top 10 messages
+// io.on("connection", (socket) => {
+//    console.log("a user connected");
+//    socket.on("chat message", (msg) => {
+//       db.createSocketMessage(JSON.parse(msg))
+//          .then((_) => {
+//             emitMostRecentMessages();
+//          })
+//          .catch((err) => io.emit(err));
+// });
+
+// close event when user disconnects from app
+//    socket.on("disconnect", () => {
+//       console.log("user disconnected");
+//    });
+// });
+
+// Displays in terminal which port the socketPort is running on
+// server.listen(socketPort, () => {
+//    console.log(`listening on *:${socketPort}`);
+// });
