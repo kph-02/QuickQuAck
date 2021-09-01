1b2190fd0372c52dcedd3c999a96f5e2080ed538
jillsu1114 <jillsu1114@gmail.com>
2021-09-01 10:56:49 -0700

    Poll Functionality for Activity and Filter Feeds

--- QQLogin/components/TagModal.js
+++ QQLogin/components/TagModal.js
@@ -256,7 +256,7 @@ const TagModal = ({ navigation }) => {
           setSelectedId(item.post_id);
 
           //navigate to post view page, sends through post information as parameter
-          navigation.navigate('Post View', { post: item });
+          navigation.navigate('Post View', { post: item, votedBool: item.has_voted ? true : false });
         }}
         backgroundColor={{ backgroundColor }}
         textColor={{ color }}
